62ee1f1b944c96d32676f8f2cf909922fe882ff9
Travelle Whitney <travellewhitney@gmail.com>
2024-07-29 19:58:59 -0500

    Adds mylooplab.md

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+function writeCards(names, gift) {
+    let thankYouMessages = [];
+  
+    names.forEach(name => {
+      let message = `Thank you, ${name}, for the wonderful ${gift} gift!`;
+      thankYouMessages.push(message);
+    });
+  
+
+    return thankYouMessages;
+
+  }
+
+  function countDown(startNumber) {
+    for (let i = startNumber; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+
+  
